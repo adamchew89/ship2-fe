39c1cc90a218cb4b01c96d79ad9e51fd505e3542
Adam Chew <adam.chew@sparescnx.com>
2022-02-06 14:11:15 +0800

    feat: added pwa

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,8 @@ import { store } from "z@Stores/store";
 import theme from "z@Themes/theme";
 import App from "./App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -25,4 +26,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
